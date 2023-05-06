f6aa22ac5067b6e20c569c3f34fa62a44673be9e
Mir0Va <mirovalo@hotmail.com>
2023-05-06 17:45:33 +0300

    added ESLint

--- models/person.js
+++ models/person.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose")
 
-mongoose.set('strictQuery',false)
+mongoose.set("strictQuery",false)
 
 const url = process.env.MONGODB_URI
 
 mongoose.connect(url)
-  .then(result =>{
+  .then(() => {
     console.log("connected to MongoDB")
   })
   .catch((error) => {
@@ -13,30 +13,30 @@ mongoose.connect(url)
   })
 
 const personSchema = new mongoose.Schema({
-    name: {
-      type: String,
-      minLength: 3,
-      required: true            
-    },
-    number: {
-      type: String,
-      minLength: 8,
-      validate: {
-        validator: function(v) {
-          return /\d{2,3}-\d*$/.test(v);
-        },
-        message: props => `${props.value} is not a valid phone number!`
+  name: {
+    type: String,
+    minLength: 3,
+    required: true
+  },
+  number: {
+    type: String,
+    minLength: 8,
+    validate: {
+      validator: function(v) {
+        return /\d{2,3}-\d*$/.test(v)
       },
-      required: true
-    }
+      message: props => `${props.value} is not a valid phone number!`
+    },
+    required: true
+  }
 })
 
-personSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-      returnedObject.id = returnedObject._id.toString()
-      delete returnedObject._id
-      delete returnedObject.__v
-    }
+personSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
 })
 
-module.exports = mongoose.model('Person', personSchema)
+module.exports = mongoose.model("Person", personSchema)
